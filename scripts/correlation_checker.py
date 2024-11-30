46a44fe9e29c7825b164905190f4c560f7dcd9e1
Mirvari Mammadli <mirvari.mammadli@gmail.com>
2024-11-30 20:21:41 +0400

    Histogram added and Correlation matrix created and filling NaN values

---
+++ scripts/correlation_checker.py
@@ -0,0 +1,38 @@
+import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
+
+stock_data = pd.read_csv("data/stocks/AAPL.csv")
+crypto_data = pd.read_csv("data/crypto/bitcoin.csv")
+
+merged_data = pd.concat(
+    [stock_data["Close"], crypto_data["price"]],
+    axis=1,
+    keys=["AAPL_Close", "BTC_Close"],
+)
+
+
+correlation_matrix = merged_data.corr()
+
+# correlation between Apple stock price and bitcoin
+plt.figure(figsize=(8, 6))
+sns.heatmap(correlation_matrix, annot=True, cmap="coolwarm")
+plt.title("Correlation Matrix Between Stock and Crypto Prices")
+plt.show()
+
+
+
+print("Missing values in stock data:")
+print(stock_data.isnull().sum())
+
+print("\nMissing values in crypto data:")
+print(crypto_data.isnull().sum())
+
+# Filling the missing values with forward-fill
+stock_data.fillna(method="ffill", inplace=True)
+crypto_data.fillna(method="ffill", inplace=True)
+
+
+print("\nMissing values after handling:")
+print(stock_data.isnull().sum())
+print(crypto_data.isnull().sum())
--- scripts/plot_drawer.py
+++ scripts/plot_drawer.py
@@ -21,3 +21,21 @@ plt.xlabel("Date")
 plt.ylabel("Price (USD)")
 plt.legend()
 plt.show()
+
+#Histogram STock Prices
+plt.hist(stock_data["Close"], bins=50, alpha=0.7, label="AAPL Closing Price")
+plt.title("Distribution of AAPL Stock Prices")
+plt.xlabel("Price (USD)")
+plt.ylabel("Frequency")
+plt.legend()
+plt.show()
+
+
+#HGistogram Crypto Prices
+plt.figure(figsize=(10, 5))
+plt.hist(crypto_data["price"], bins=50, alpha=0.7, color="orange", label="BTC Closing Price")
+plt.title("Distribution of BTC Cryptocurrency Prices")
+plt.xlabel("Price (USD)")
+plt.ylabel("Frequency")
+plt.legend()
+plt.show()
